9bd9a0f282cb7563a6405b3e3e2a01f41b1ccadd
jack <daisy.zhao@flipscript.com.cn>
2017-09-14 18:13:53 +0800

    init

---
+++ first-demo/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+   <h1 @click="getshow">{{title}}</h1>
+   <input v-model="title">
+   <ul>
+    <li v-for="name in fruits">
+      {{name.name}}
+    </li>
+   </ul>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  data:function(){
+    return {
+      title:'my name is yanghao',
+      fruits:[
+        {name:"apple"},
+        {name:"banana"},
+        {name:"tool"}
+      ]
+    
+    }
+  },
+  methods:{
+    getshow: function(){
+      this.title=this.title+'xiamawen'
+    }
+  }
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ simple/learntypescript/first.ts
@@ -0,0 +1,4 @@
+function say(text:string){
+    console.log(text)
+}
+say('hello')
